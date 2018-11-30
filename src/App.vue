e4ea50b41a4eacc20b34efeaa700177e4fa0fe98
zkc1995 <1486148673@qq.com>
2018-11-30 22:09:42 +0800

    2018年11月30日22:09:39

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 头部导航栏 -->
+    <header id="header">
+      <ul>
+        <li class="active"><a href="#">推荐音乐</a></li>
+        <li><a href="#">热歌榜</a></li>
+        <li><a href="#">搜索</a></li>
+      </ul>
+    </header>
+    <router-view></router-view>
+    <!-- 底部版权信息 -->
+    <footer id="footer">
+      皮皮版权所有©2018
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入样式
+import './assets/main.css'
+
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+// 实例路由对象
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+
+
 new Vue({
   render: h => h(App),
+  // 挂载根实例
+  router
 }).$mount('#app')
